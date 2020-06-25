0d2e43bed8aae49e085c2ad317d7b8566e3ab5b5
Thomas Rausch <thomas@rauschig.org>
2020-06-25 16:28:41 +0200

    Release version 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="edgerun-ether",
-    version="0.1.0",
+    version="0.2.0",
     author="Thomas Rausch",
     author_email="t.rausch@dsg.tuwien.ac.at",
     description="Ether - Synthesize plausible edge infrastructure topologies",
