8ac634168b793ce1b4fb8a07ca585f80edfb7c82
Andreas Bruckner <andreas@bruckner.cc>
2020-11-26 13:14:13 +0100

    Refactor protocol

--- examples/simulation/messaging.py
+++ examples/simulation/messaging.py
@@ -4,6 +4,7 @@ from itertools import product
 import numpy as np
 
 from ether import vivaldi
+from ether.vivaldi import VivaldiCoordinate
 from examples.simulation.protocol import *
 from examples.vivaldi.client_experiment import ClientExperiment
 from examples.vivaldi.urban_sensing import create_topology
@@ -20,25 +21,18 @@ def client(env: simpy.Environment, protocol: Protocol, node: Node, initial_broke
     def ping_brokers(brokers: List[Node]):
         for (b, _) in product(brokers, range(5)):
             protocol.send(node, b, PingMessage())
-            (source, reply) = yield protocol.receive(node)
+            reply = yield protocol.receive(node)
             if isinstance(reply, PongMessage):
-                latency = env.now - reply.timestamp
-                vivaldi.execute(node, source, latency * 2)
+                vivaldi.execute(node, reply.source, reply.latency * 2)
 
     def ping_random(n=5):
-        protocol.send(node, selected_broker, FindRandomBrokersRequest())
-        (_, message) = yield protocol.receive(node)
-
-        if not isinstance(message, FindRandomBrokersResponse):
-            raise AssertionError(f'unexpected message received: {message}')
+        yield protocol.send(node, selected_broker, FindRandomBrokersRequest())
+        message = yield protocol.receive(node)
         yield from ping_brokers(message.brokers[:n])
 
     def ping_closest(n=5):
-        protocol.send(node, selected_broker, FindClosestBrokersRequest())
-        (_, message) = yield protocol.receive(node)
-
-        if not isinstance(message, FindClosestBrokersResponse):
-            raise AssertionError(f'unexpected message received: {message}')
+        yield protocol.send(node, selected_broker, FindClosestBrokersRequest())
+        message = yield protocol.receive(node)
         yield from ping_brokers(message.brokers[:n])
 
     while True:
@@ -47,21 +41,19 @@ def client(env: simpy.Environment, protocol: Protocol, node: Node, initial_broke
         yield env.timeout(60_000)
 
 
-def broker(env: simpy.Environment, protocol: Protocol, node: Node, brokers: List[Node]):
+def broker(protocol: Protocol, node: Node, brokers: List[Node]):
     while True:
-        source: Node
-        message: Message
-        (source, message) = yield protocol.receive(node)
+        message = yield protocol.receive(node)
         assert isinstance(node.coordinate, VivaldiCoordinate)
         if isinstance(message, PingMessage):
-            response = PongMessage(node.coordinate, env.now)
+            response = PongMessage()
         elif isinstance(message, FindRandomBrokersRequest):
             response = FindRandomBrokersResponse(random.choices(brokers, k=5))
         elif isinstance(message, FindClosestBrokersRequest):
-            response = FindClosestBrokersResponse(sorted(brokers, key=lambda b: source.distance_to(b))[:5])
+            response = FindClosestBrokersResponse(sorted(brokers, key=lambda b: message.source.distance_to(b))[:5])
         else:
             continue
-        protocol.send(node, source, response)
+        yield protocol.send(node, message.source, response)
 
 
 def main():
@@ -71,10 +63,10 @@ def main():
     execute_vivaldi(topology, node_filter=lambda n: 'broker' in n.name, min_executions=300)
     env = simpy.Environment()
     proto = Protocol(env, topology)
-    for c in (n for n in topology.get_nodes() if 'client' in n.name):
-        env.process(client(env, proto, c, random.choice(brokers)))
-    for b in (n for n in topology.get_nodes() if 'broker' in n.name):
-        env.process(broker(env, proto, b, brokers))
+    for c in clients:
+        env.process(client(env, proto, c, brokers[0]))
+    for b in brokers:
+        env.process(broker(proto, b, brokers))
 
     experiment = ClientExperiment(topology, clients, brokers)
 
--- examples/simulation/protocol.py
+++ examples/simulation/protocol.py
@@ -1,15 +1,24 @@
+import math
 from collections import defaultdict
-from typing import List, Dict
+from typing import List, Dict, TypeVar, Callable, Any, Generator, Type
 
 import simpy
 
 from ether.core import Node
 from ether.topology import Topology
-from ether.vivaldi import VivaldiCoordinate
 
 
 class Message:
-    pass
+    source: Node
+    destination: Node
+    timestamp: int
+    latency: float
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self.__dict__})'
+
+
+MessageT = TypeVar('MessageT', bound=Message)
 
 
 class PingMessage(Message):
@@ -17,12 +26,7 @@ class PingMessage(Message):
 
 
 class PongMessage(Message):
-    coordinate: VivaldiCoordinate
-    timestamp: int
-
-    def __init__(self, coordinate: VivaldiCoordinate, timestamp: int):
-        self.coordinate = coordinate
-        self.timestamp = timestamp
+    pass
 
 
 class FindClosestBrokersRequest(Message):
@@ -47,27 +51,82 @@ class FindRandomBrokersResponse(Message):
         self.brokers = brokers
 
 
+class Connect(Message):
+    broker: Node
+
+    def __init__(self, broker: Node):
+        self.broker = broker
+
+
+class ConnectAck(Message):
+    pass
+
+
+class Sub(Message):
+    topic: str
+
+    def __init__(self, topic: str):
+        self.topic = topic
+
+
+class SubAck(Message):
+    pass
+
+
+class Unsub(Message):
+    topic: str
+
+    def __init__(self, topic: str):
+        self.topic = topic
+
+
+class UnsubAck(Message):
+    pass
+
+
+class Pub(Message):
+    topic: str
+    data: Any
+
+    def __init__(self, topic: str, data: Any):
+        self.topic = topic
+        self.data = data
+
+
+class PubAck(Message):
+    pass
+
+
+class Shutdown(Message):
+    pass
+
+
 class Protocol(object):
     env: simpy.Environment
-    stores: Dict[Node, simpy.Store]
+    stores: Dict[Node, simpy.FilterStore]
     topology: Topology
+    history: List[Message]
+    enable_ack: bool
 
-    def __init__(self, env: simpy.Environment, topology: Topology):
+    def __init__(self, env: simpy.Environment, topology: Topology, enable_ack=True):
         self.env = env
-        self.stores = defaultdict(lambda: simpy.Store(env))
+        self.stores = defaultdict(lambda: simpy.FilterStore(env))
         self.topology = topology
+        self.history = list()
+        self.enable_ack = enable_ack
 
-    def send(self, source, destination, message):
-        self.env.process(self._do_send(source, destination, message))
+    def send(self, source: Node, destination: Node, message: MessageT):
+        message.source = source
+        message.destination = destination
+        message.timestamp = self.env.now
+        message.latency = self.topology.latency(source, destination)
+        self.history.append(message)
+        return simpy.AllOf(self.env, [
+            self.env.timeout(int(math.ceil(message.latency))),
+            self.stores[message.destination].put(message)
+        ])
 
-    def _do_send(self, source: Node, destination: Node, message: Message):
-        yield self.env.timeout(int(self.topology.route(source, destination).rtt))
-        self.stores[destination].put((source, message))
-
-    def receive(self, node: Node):
+    def receive(self, node: Node, *message_types: Type[MessageT]):
+        if len(message_types) > 0:
+            return self.stores[node].get(lambda m: type(m) in message_types)
         return self.stores[node].get()
-
-    def has_item(self, node: Node):
-        return len(self.stores[node].items) > 0
-
-
