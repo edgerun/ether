19e5ab48e7307c2d107325cc3e670bad18024c5c
Philipp <poepsi39@gmail.com>
2021-03-17 17:41:16 +0100

    Add new node factories

--- ether/blocks/nodes.py
+++ ether/blocks/nodes.py
@@ -65,6 +65,68 @@ def create_tx2_node(name=None) -> Node:
                        })
 
 
+def create_rockpi(name=None) -> Node:
+    name = name if name is not None else 'rockpi_%d' % next(counters['rockpi'])
+
+    return create_node(name=name,
+                       cpus=6, arch='aarch64', mem='4G',
+                       labels={
+                           'ether.edgerun.io/type': 'sbc',
+                           'ether.edgerun.io/model': 'rockpi4'
+                       })
+
+
+def create_rpi4_node(name=None) -> Node:
+    name = name if name is not None else 'rpi4_%d' % next(counters['rpi4'])
+
+    return create_node(name=name,
+                       arch='arm32v7',
+                       cpus=4,
+                       mem='1G',
+                       labels={
+                           'ether.edgerun.io/type': 'sbc',
+                           'ether.edgerun.io/model': 'rpi4',
+                       })
+
+
+def create_coral(name=None) -> Node:
+    name = name if name is not None else 'coral_%d' % next(counters['coral'])
+
+    return create_node(name=name,
+                       cpus=4, arch='aarch64', mem='1G',
+                       labels={
+                           'ether.edgerun.io/type': 'sbc',
+                           'ether.edgerun.io/model': 'coral',
+                           'ether.edgerun.io/capabilities/tpu': 'edgetpu',
+    })
+
+
+def create_nano(name=None) -> Node:
+    name = name if name is not None else 'nano_%d' % next(counters['nano'])
+
+    return create_node(name=name,
+                       cpus=4, arch='aarch64', mem='4G',
+                       labels={
+                           'ether.edgerun.io/type': 'embai',
+                           'ether.edgerun.io/model': 'nvidia_jetson_nano',
+                           'ether.edgerun.io/capabilities/cuda': '10',
+                           'ether.edgerun.io/capabilities/gpu': 'maxwell',
+                       })
+
+
+def create_nx(name=None) -> Node:
+    name = name if name is not None else 'nx_%d' % next(counters['nx'])
+
+    return create_node(name=name,
+                       cpus=6, arch='aarch64', mem='8G',
+                       labels={
+                           'ether.edgerun.io/type': 'embai',
+                           'ether.edgerun.io/model': 'nvidia_jetson_nx',
+                           'ether.edgerun.io/capabilities/cuda': '10',
+                           'ether.edgerun.io/capabilities/gpu': 'volta',
+                       })
+
+
 def create_node(name: str, cpus: int, mem: str, arch: str, labels: Dict[str, str]) -> Node:
     capacity = Capacity(cpu_millis=cpus * 1000, memory=parse_size_string(mem))
     return Node(name, capacity=capacity, arch=arch, labels=labels)
@@ -74,3 +136,9 @@ rpi3 = create_rpi3_node
 nuc = create_nuc_node
 tx2 = create_tx2_node
 server = create_server_node
+nx = create_nx
+nano = create_nano
+coral = create_coral
+rpi4 = create_rpi4_node
+rockpi = create_rockpi
+
